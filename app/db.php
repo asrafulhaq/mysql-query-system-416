4e51209e1526e53daee0d550ae46fc0519e30d03
haq <haq@gmail.com>
2020-07-10 10:59:02 +0600

    status add done

---
+++ app/db.php
@@ -0,0 +1,23 @@
+<?php  
+	
+
+	/**
+	 * Database connection 
+	 */
+
+	$host = 'localhost';
+	$user = 'root';
+	$pass = '';
+	$db = 'awd416';
+
+	$connection = new mysqli($host, $user, $pass, $db);
+
+
+	
+
+
+
+
+
+
+?>
